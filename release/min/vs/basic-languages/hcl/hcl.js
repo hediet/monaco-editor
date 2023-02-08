a69d5099f3bbd9b028a7cf549925f23cb533beda
hediet <hediet@users.noreply.github.com>
2023-02-08 14:15:29 +0000

    deploy: fdd81a9401fff5b3842f030d1e86b624c77b5e69

--- release/min/vs/basic-languages/hcl/hcl.js
+++ release/min/vs/basic-languages/hcl/hcl.js
@@ -1,6 +1,6 @@
 "use strict";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.34.1(478c324f7046c0ab60415d732a0687b384a02225)
+ * Version: 0.34.1(fdd81a9401fff5b3842f030d1e86b624c77b5e69)
  * Released under the MIT license
  * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
  *-----------------------------------------------------------------------------*/
